d5fdd1ac6c4948ec949bd8a80d0158ae9ccd9066
Thiago-Cardoso <thiagocardosooti@gmail.com>
2025-11-23 19:14:51 -0300

    feat(mobile): add account management screens

---
+++ mobile/src/app/accounts/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Accounts Module Exports
+ */
+
+export { AccountListView } from './AccountList.view';
+export { AccountFormView } from './AccountForm.view';
+export { AccountCard, AccountCardSkeleton } from './components/AccountCard';
+export { TotalBalanceCard } from './components/TotalBalanceCard';
